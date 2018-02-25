84615f636f1f8ac1305ebbc960e9987141ee8072
shocci1214 <shocci1214@gmail.com>
2018-02-25 20:40:33 +0900

    game2(=stage2)を追加

---
+++ Scene/scene_game2/Director.rb
@@ -0,0 +1,47 @@
+require_relative '../Player'
+require_relative '../Enemy'
+require_relative '../info/Timer'
+require_relative '../info/Score'
+
+module Game
+    class Director
+        attr_accessor :timer
+        #敵の出現用変数
+        @@count = 0
+        #プレイヤーの弾
+        @@playershot = PlayerShot.get_playershot
+
+        def initialize
+            @player = Player.new
+            @timer = Timer.new(30)
+            @score = Score.new(0)
+        end
+
+
+        def play
+            @player.update
+            @timer.update
+            @score.update
+            Sprite.update([@@playershot,$Enemies])
+
+            #敵の出現
+            @@count += 1
+            if @@count % 100 == 0
+                $Enemies << Enemy.new
+            end
+
+            #プレイヤーの弾と敵機の衝突判定
+            #衝突したとき、$PlayerShotのshotメソッドと、$enemyのhitメソッドを呼び出す
+            if Sprite.check(@@playershot,$Enemies)
+                @score.addScore
+                EXPLOSION_SOUND.play
+            end
+
+            #ゲームオーバー
+            if @timer.time < 0
+                BGM.stop
+                Scene.move_to(:gameover)
+            end
+        end
+    end
+end
